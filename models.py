02741a0464f9a93e3582b7af43abebe3092ed154
frodoman <my_adress_2003@hotmail.com>
2020-08-23 22:40:39 +0100

    Add 3 movies to database.

--- models.py
+++ models.py
@@ -10,17 +10,6 @@ database_name = "casting_agency_app"
 local_db_path = "postgres://{}/{}".format('XinghouLiu@localhost:5432', database_name)
 db = SQLAlchemy()
 
-'''
-setup_db(app)
-    binds a flask application and a SQLAlchemy service
-
-def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.app = app
-    db.init_app(app)
-    db.create_all()
-'''
 
 def set_up_db(app):
     app.config.from_object('config')
@@ -60,6 +49,8 @@ class Actors(db.Model):
 #  Reference table between Movie and Actor, 
 #  which is Many to Many relationship
 #  ----------------------------------------------------------------  
+
+
 movie_actor = db.Table('movie_actor', 
     db.Column('movie_id', db.Integer, db.ForeignKey('Movies.id'), primary_key=True),
     db.Column('actor_id', db.Integer, db.ForeignKey('Actors.id'), primary_key=True)
